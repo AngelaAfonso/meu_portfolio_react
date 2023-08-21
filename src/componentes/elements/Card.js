2994866e49444d9fa00bc30234e9c7bebd3491f1
AngelaAfonso <angelareisafo2020@gmail.com>
2023-08-21 20:59:46 -0300

    versao-2

--- src/componentes/elements/Card.js
+++ src/componentes/elements/Card.js
@@ -1,18 +1,31 @@
 import styles from './Card.module.css'
 import ButtonB from './ButtonB'
+import { useState } from 'react'
 
-function Card({img,title,tech,description,repo,site}){
-    return(
-        <div className={styles.card}>
-            <a href={site}>
-            <img src={img} alt="ERRO"></img>
+function Card({ img, title, tech, description, repo, site }) {
+    const [info, setInfo] = useState(false)
+
+    function InfoOn() {
+        setInfo(true)
+    }
+    function InfoOff() {
+        setInfo(false)
+    }
+
+    return (
+        <div onMouseOut={InfoOff} className={styles.card}>
+            <a onMouseEnter={InfoOn} href={site}>
+                <img src={img} alt="ERRO"></img>
             </a>
-            <section>
-                <h3>{title}</h3>
-                <p><strong>Tecnologia</strong>{tech}</p>
-                <p>{description}</p>
-                <ButtonB text='Acesse o repositório' link={repo}/>
-            </section>
+
+            {info === true && (
+                <section>
+                    <h3>{title}</h3>
+                    <p><strong>Tecnologia</strong>{tech}</p>
+                    <p>{description}</p>
+                    <ButtonB text='Acesse o repositório' link={repo} />
+                </section>
+            )}
         </div>
     )
 }
--- src/componentes/sections/Presentation.js
+++ src/componentes/sections/Presentation.js
@@ -1,12 +1,43 @@
 import styles from './Presentation.module.css';
 import ButtonA from '../elements/ButtonA';
 import ButtonB from '../elements/ButtonB';
+import { useEffect, useState } from 'react'
 
-function Presentation(){
-    return(
+function Presentation() {
+    const [text, setText] = useState('')
+    const toRotate = ['Angela Afonso', 'Desenvolvedora Front-End']
+    const [loop, setLoop] = useState(0)
+    const [isDeleting, setIsDeleting] = useState(false);
+    const period = 100;
+    const [delta, setDelta] = useState(100)
+
+    useEffect(() => {
+        let ticker = setInterval(() => {
+            toType()
+        }, delta)
+        return () => { clearInterval(ticker) }
+    }, [text])
+
+    const toType = ()=>{
+        let i = loop % toRotate.length;
+        let fullText = toRotate[i]
+        let updatedText = isDeleting ? fullText.substring(0,text.length-1) : fullText.substring(0,text.length+1)
+
+        setText(updatedText);
+        if(!isDeleting && updatedText === fullText){
+            setIsDeleting(true);
+            setDelta(period);
+        }else if (isDeleting && updatedText === ''){
+            setIsDeleting(false);
+            setDelta(period);
+            setLoop(loop+1);
+        }
+     }
+
+    return (
         <div className={styles.presentation} id="presentation">
             <h4><strong>Bem-vindo(a) ao meu Portfólio</strong></h4>
-            <h1>Olá! Eu sou a Angela Afonso</h1>
+            <h1>Olá! Eu sou {text} </h1>
             <p>
                 Sou uma apaixonada por tecnologia e soluções inovadoras. <br />
                 Eu tenho o compromisso de resolver problemas <br />
@@ -14,10 +45,10 @@ function Presentation(){
                 Estou sempre em busca de novos desafios para superar.
             </p>
 
-            <ButtonA text='Conecte-se comigo!' link='https://github.com/AngelaAfonso'/>
+            <ButtonA text='Conecte-se comigo!' link='https://github.com/AngelaAfonso' />
         </div>
     )
-        
+
 }
 
 export default Presentation
